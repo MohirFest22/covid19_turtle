30f191a7a02d181fcba06e4c51e6626835e4a47a
Arslonov Sunnatjon <118212317+MohirFest22@users.noreply.github.com>
2023-07-26 14:37:25 +0500

    Add files via upload
    Finally, Program ready!!!

---
+++ covid19/covid19.py.py
@@ -0,0 +1,19 @@
+from turtle import *
+
+tk = Screen()
+tk.title("shakl")
+tk.bgcolor("black")
+tk.setup(700,800)
+a,b=0,0
+color("limegreen")
+for i in range(200):
+	hideturtle()
+	speed(140)
+	fd(a)
+	rt(b)
+	a+=3
+	b+=1
+
+
+
+tk.mainloop()
